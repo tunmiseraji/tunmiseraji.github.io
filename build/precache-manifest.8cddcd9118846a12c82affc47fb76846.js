7fd877b6e04996dfc8fc3c6da74496dae4153df3
tunmiseraji <rajitunmise@gmail.com>
2024-10-01 09:20:11 -0400

    Add build folder

---
+++ build/precache-manifest.8cddcd9118846a12c82affc47fb76846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211dc8e88793af3fc55f966b255e3cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe6a94ebe2c50fa87006",
+    "url": "/static/css/main.28bba7a7.chunk.css"
+  },
+  {
+    "revision": "38180bf066f7d7056d44",
+    "url": "/static/js/2.5a3a7e87.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5a3a7e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6a94ebe2c50fa87006",
+    "url": "/static/js/main.3e9c6da3.chunk.js"
+  },
+  {
+    "revision": "c36ce27fe921ac8b2570",
+    "url": "/static/js/runtime-main.622b5d1c.js"
+  }
+]);
